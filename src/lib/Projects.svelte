01c164ce91bd90e03d50526a41ac1c5e31c092f2
Sven Gaubert <svengau@gmail.com>
2022-08-23 07:36:04 +0200

    fix null values

--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -11,7 +11,7 @@
             <div class="flex flex-col">
               <div class="flex flex-row gap-4 text-gray-500 text-sm" />
               <h2 class="text-xl">{item.name}</h2>
-              <div>{item.description}</div>
+              <div>{item.description || ""}</div>
               <div class="mt-2 text-gray-500 ">
                 <div>{item.topics.map((tag) => `#${tag}`).join(" ")}</div>
                 <div class="flex flex-row gap-6">
@@ -19,7 +19,7 @@
                     {item.created_at.substring(0, 4)}
                   </div>
                   <div>
-                    {item.language}
+                    {item.language || ""}
                   </div>
                   {#if item.stargazers_count}
                     <div>
